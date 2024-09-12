fe172ee817bb294c2964de93da6f08feb6a9d3d6
Nikita <perfect1201@ukr.net>
2024-09-12 19:18:26 +0300

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact, { rules } from 'eslint-plugin-react';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
   { rules: { 'react/react-in-jsx-scope': 'off' } },
