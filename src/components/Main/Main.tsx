5995aa3f161f5ed9217de0393f3ad893f2f993c9
Nikita <perfect1201@ukr.net>
2024-09-13 18:05:09 +0300

    final vers

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import './Main.scss';
-import { Header } from '../Header';
-import { MainBody } from './MainBody';
-import { Footer } from '../Footer';
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 type Props = {};
 
 export const Main: React.FC<Props> = () => {
-  return (
-    <>
-      <Header />
-      <MainBody />
-      <Footer />
-    </>
-  );
+  return <div className="main-content"></div>;
 };
--- src/components/Main/MainBody/MainBody.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './MainBody.scss';
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-type Props = {};
-
-export const MainBody: React.FC<Props> = () => {
-  return <></>;
-};
