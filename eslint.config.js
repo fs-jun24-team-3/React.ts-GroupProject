ae3bad4ac768495b2c0d5085ca3f5c8bdb38e397
Nikita <perfect1201@ukr.net>
2024-09-13 14:38:37 +0300

    eslint start over

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact, { rules } from 'eslint-plugin-react';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
   { rules: { 'react/react-in-jsx-scope': 'off' } },
@@ -11,4 +11,16 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {ignores: ['build/', 'dist/assets/index-CnMgUSrV.js', 'node_modules']},
 ];
