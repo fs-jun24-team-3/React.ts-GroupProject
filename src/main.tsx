2356ae6632b4fb9c01959364520c96451d907b11
Nikita <perfect1201@ukr.net>
2024-09-14 18:27:48 +0300

    small correction

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 import { Root } from './Root.tsx';
 
-createRoot(document.getElementById('root')!).render(<Root />);
+createRoot(document.getElementById('root') as HTMLElement).render(<Root />);
