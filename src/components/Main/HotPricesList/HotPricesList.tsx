f7986da4e21612242d370943ec479ba8e8595e70
Nikita <perfect1201@ukr.net>
2024-09-14 14:39:58 +0300

    applied suggestion

--- src/components/Main/HotPricesList/HotPricesList.tsx
+++ src/components/Main/HotPricesList/HotPricesList.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GoodsSlider } from '../GoodsSlider';
-import { PhoneCard } from '../PhoneCard';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 type Props = {};
