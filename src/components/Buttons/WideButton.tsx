c49f09c6999946eaef52dca099e4dab6af64fdc7
Nikita <perfect1201@ukr.net>
2024-09-16 13:51:03 +0300

    now you can configure size of WideButton (optionally)

--- src/components/Buttons/WideButton.tsx
+++ src/components/Buttons/WideButton.tsx
@@ -4,14 +4,22 @@ import './Buttons.scss';
 type Props = {
   onClick?: () => void;
   buttonTitle: string;
+  width?: number;
+  height?: number;
 };
 
 export const WideButton: React.FC<Props> = ({
   buttonTitle,
+  height,
+  width,
   onClick = () => {},
 }) => {
   return (
-    <button className="wide-button" onClick={onClick}>
+    <button
+      className="wide-button"
+      onClick={onClick}
+      style={{ height: height, width: width }}
+    >
       {buttonTitle}
     </button>
   );
