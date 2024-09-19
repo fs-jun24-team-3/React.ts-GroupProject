6c7402caf00b08c6a6e6ab06c71630a4a3643f47
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-09-19 15:32:58 +0300

    add grid and title

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import styles from './PhonesPage.module.scss';
+import { PhoneCard } from '../../components/Main/PhoneCard';
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 type Props = {};
 
 export const PhonesPage: React.FC<Props> = () => {
-  return <>Phones page</>;
+  return (
+    <>
+      <div className={styles.phones}>
+        <div className={styles.phones__routs}>
+          <div className={styles.phones__routs__home}></div>
+          <div className={styles.phones__routs__array}></div>
+          <div className={styles.phones__routs__currentPage}>Phones</div>
+        </div>
+        <div className={styles.phones__title}>Mobile phones</div>
+        <div className={styles.phones__countModel}>95 models</div>
+        <div className={styles.phonesDropdown}>Компонента Олени</div>
+        <div className={styles.phones__items}>
+          <div className={styles.phones__item}>
+            <PhoneCard />
+          </div>
+          <div className={styles.phones__item}>
+            <PhoneCard />
+          </div>
+          <div className={styles.phones__item}>
+            <PhoneCard />
+          </div>
+          <div className={styles.phones__item}>
+            <PhoneCard />
+          </div>
+          <div className={styles.phones__item}>
+            <PhoneCard />
+          </div>
+          <div className={styles.phones__item}>{/* <PhoneCard /> */}</div>
+          <div className={styles.phones__item}>{/* <PhoneCard /> */}</div>
+          <div className={styles.phones__item}>{/* <PhoneCard /> */}</div>
+          <div className={styles.phones__item}>{/* <PhoneCard /> */}</div>
+          <div className={styles.phones__item}>{/* <PhoneCard /> */}</div>
+          {/* <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard />
+          <PhoneCard /> */}
+        </div>
+      </div>
+    </>
+  );
 };
