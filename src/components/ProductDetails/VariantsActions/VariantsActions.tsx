ba1f8e4a56a605970610111c634e1bd66a65b1a0
Kate Marchuk <pion3219@gmail.com>
2024-09-19 21:50:54 +0300

    Added Product details page layout and route

---
+++ src/components/ProductDetails/VariantsActions/VariantsActions.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './VariantsActions.module.scss';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+type Props = {};
+
+export const VariantsActions: React.FC<Props> = () => {
+  return <div className={styles['variants-block']}>Variants Actions</div>;
+};
