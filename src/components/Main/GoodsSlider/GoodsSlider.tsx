6996f2f80193d427328708c3da69dcdca520012c
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-09-19 21:39:40 +0300

    add api data on catalog page and pagination

--- src/components/Main/GoodsSlider/GoodsSlider.tsx
+++ src/components/Main/GoodsSlider/GoodsSlider.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import { PhoneCard } from '../PhoneCard';
 import './GoodSlider.scss';
+import { Phone } from '../../../utils/types/Phone';
 
 type Props = {
   sliderTitle: string;
+};
+
+const phones: Phone = {
+  id: 'apple-iphone-11-128gb-black',
+  category: 'phones',
+  namespaceId: 'apple-iphone-11',
+  name: 'Apple iPhone 11 128GB Black',
+  capacityAvailable: ['64GB', '128GB', '256GB'],
+  capacity: '128GB',
+  priceRegular: 1100,
+  priceDiscount: 1050,
+  colorsAvailable: ['black', 'green', 'yellow', 'white', 'purple', 'red'],
+  color: 'black',
+  images: [
+    'img/phones/apple-iphone-11/black/00.webp',
+    'img/phones/apple-iphone-11/black/01.webp',
+    'img/phones/apple-iphone-11/black/02.webp',
+    'img/phones/apple-iphone-11/black/03.webp',
+    'img/phones/apple-iphone-11/black/04.webp',
+  ],
+  description: [
+    { title: 'Display', text: "6.1' IPS LCD" },
+    { title: 'Camera', text: '12MP ultra-wide and 12MP wide cameras' },
+    { title: 'Battery', text: '3110 mAh battery with fast charging' },
+  ],
+  screen: "6.1' IPS",
+  resolution: '1792x828',
+  processor: 'Apple A13 Bionic',
+  ram: '4GB',
+  camera: '12 Mp + 12 Mp + 12MP',
+  zoom: 'Digital, 5x',
+  cell: ['GPRS', 'EDGE', 'WCDMA', 'UMTS', 'HSPA', 'LTE'],
 };
 
 export const GoodsSlider: React.FC<Props> = ({ sliderTitle }) => {
@@ -24,10 +57,10 @@ export const GoodsSlider: React.FC<Props> = ({ sliderTitle }) => {
       </div>
       <ul className="slider__list">
         {/* TODO: at this stage we need to load phone cards from server, map given list and render it */}
-        <PhoneCard />
-        <PhoneCard />
-        <PhoneCard />
-        <PhoneCard />
+        <PhoneCard phones={phones} />
+        <PhoneCard phones={phones} />
+        <PhoneCard phones={phones} />
+        <PhoneCard phones={phones} />
       </ul>
     </div>
   );
--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './PhonesPage.module.scss';
 import { PhoneCard } from '../../components/Main/PhoneCard';
+import { loadPhones } from '../../app/slices/phonesSlice';
+
+import { RootState } from '../../app/store';
+import { useAppDispatch, useAppSelector } from '../../app/reduxHooks';
+import { Phone } from '../../utils/types/Phone';
+
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 type Props = {};
 
+const PHONES_PER_PAGE = 12;
+
 export const PhonesPage: React.FC<Props> = () => {
+  const dispatch = useAppDispatch();
+  const phones = useAppSelector((state: RootState) => state.phones.phones);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(phones.length / PHONES_PER_PAGE);
+
+  const indexOfLastPhone = currentPage * PHONES_PER_PAGE;
+  const indexOfFirstPhone = indexOfLastPhone - PHONES_PER_PAGE;
+  const currentPhones = phones.slice(indexOfFirstPhone, indexOfLastPhone);
+
+  useEffect(() => {
+    dispatch(loadPhones());
+  }, [dispatch]);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <>
       <div className={styles.phones}>
@@ -14,36 +39,34 @@ export const PhonesPage: React.FC<Props> = () => {
           <div className={styles.phones__routs__currentPage}>Phones</div>
         </div>
         <div className={styles.phones__title}>Mobile phones</div>
-        <div className={styles.phones__countModel}>95 models</div>
+        <div className={styles.phones__countModel}>{phones.length} models</div>
         <div className={styles.phonesDropdown}>Компонента Олени</div>
         <div className={styles.phones__items}>
-          <div className={styles.phones__item}>
-            <PhoneCard />
+          {currentPhones.map((phones: Phone) => (
+            <div className={styles.phones__item} key={phones.id}>
+              <PhoneCard phones={phones} />
+            </div>
+          ))}
+        </div>
+        <div className={styles.phones__pagination}>
+          <button className={styles.phones__pagination__arr}>
+            <div className={styles.phones__pagination__arrLeft}></div>
+          </button>
+          <div className={styles.phones__pagination__buttons}>
+            {[...Array(totalPages)].map((_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => paginate(index + 1)}
+                // className={currentPage === index + 1 ? styles.activePage : ''}
+                className={styles.phones__pagination__button}
+              >
+                {index + 1}
+              </button>
+            ))}
           </div>
-          <div className={styles.phones__item}>
-            <PhoneCard />
-          </div>
-          <div className={styles.phones__item}>
-            <PhoneCard />
-          </div>
-          <div className={styles.phones__item}>
-            <PhoneCard />
-          </div>
-          <div className={styles.phones__item}>
-            <PhoneCard />
-          </div>
-          {/* <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard />
-          <PhoneCard /> */}
+          <button className={styles.phones__pagination__arr}>
+            <div className={styles.phones__pagination__arrRight}></div>
+          </button>
         </div>
       </div>
     </>
