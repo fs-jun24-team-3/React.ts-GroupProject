fddf890c8d55b249e3b7dc28d980b5c4529f0b76
Olena Nikishyna <lyashenko.ol.yur@gmail.com>
2024-09-22 23:07:55 +0300

    added about component

--- src/components/ProductDetails/About/About.tsx
+++ src/components/ProductDetails/About/About.tsx
@@ -11,7 +11,6 @@ export const About: React.FC<Props> = ({ description }) => {
     <section className={styles.descriptionSection}>
       <h3 className={styles.sectionTitle}>About</h3>
       <div className={styles.divider}></div>
-
       {description.map((item, index) => (
         <article key={index} className={styles.descriptionSection}>
           <h4 className={styles.descriptionTitle}>{item.title}</h4>
