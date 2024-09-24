90767f05de1f92ec37bbbe4d24b3b1e33e24c936
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-09-24 11:56:37 +0300

    add layout for favorite page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,5 +1,18 @@
-import styles from './FavoritesPage.module.scss';
+import { Catalog } from '../../components/Catalog/Catalog';
+// import styles from './FavoritesPage.module.scss';
+import { useAppSelector } from '../../app/reduxHooks';
+import { RootState } from '../../app/store';
 
 export const FavoritesPage = () => {
-  return <div className={styles.favorites}>Favorites</div>;
+  //в цій компоненті я передію не данні favorites, а данні accessories(це заглушка).
+  //Нікіта, коли ти зробиш редакс для цієї компоненти, поєднаєш його з локальним хранилищем, зміни цю змінну.
+  const favorites = useAppSelector(
+    (state: RootState) => state.accessories.accessories,
+  );
+
+  return (
+    <>
+      <Catalog items={favorites} title={'Favorites'} isFiltered={false} />
+    </>
+  );
 };
