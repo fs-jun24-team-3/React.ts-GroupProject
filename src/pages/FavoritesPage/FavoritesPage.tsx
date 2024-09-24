dccb9f6be7234432d50b98650b31721564256d0f
Olexandra Pastushenko <sandorik1608@gmail.com>
2024-09-24 13:07:56 +0300

    fix conflict

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -5,7 +5,6 @@ import { useAppSelector } from '../../app/reduxHooks';
 import { RootState } from '../../app/store';
 
 export const FavoritesPage = () => {
- //comment
   //в цій компоненті я передію не данні favorites, а данні accessories(це заглушка).
   //Нікіта, коли ти зробиш редакс для цієї компоненти, поєднаєш його з локальним хранилищем, зміни цю змінну.
   const favorites = useAppSelector(
