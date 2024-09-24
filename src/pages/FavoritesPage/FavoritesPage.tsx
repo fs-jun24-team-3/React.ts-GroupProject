e797773b22efb6d68be44d8ca8acc8f90b91aaec
Olexandra Pastushenko <sandorik1608@gmail.com>
2024-09-24 11:42:19 +0300

    add breadcrumbs component

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,5 +1,11 @@
+import { Breadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs';
 import styles from './FavoritesPage.module.scss';
 
 export const FavoritesPage = () => {
-  return <div className={styles.favorites}>Favorites</div>;
+  return (
+    <div className={styles.favorites}>
+      <Breadcrumbs />
+      Favorites
+    </div>
+  );
 };
